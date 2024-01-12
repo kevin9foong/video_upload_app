b3e09617ddc2d1352cc742ad228406951265926f
Kevin Foong <kevin9foong@gmail.com>
2024-01-12 12:47:41 +0800

    Add docker compose support

--- server/database/sequelize.js
+++ server/database/sequelize.js
@@ -1,5 +1,22 @@
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
 
-const sequelize = new Sequelize('postgres://postgres:pw@localhost:5432/postgres');
+const DB_HOST = process.env.POSTGRES_DB_HOST || 'localhost';
+const DB_PORT = process.env.POSTGRES_DB_PORT || '5432';
+const DB_NAME = process.env.POSTGRES_DB_NAME || 'postgres';
+const DB_USER_NAME = process.env.POSTGRES_USER_NAME || 'postgres';
+const DB_USER_PASSWORD_FILE = process.env.POSTGRES_USER_PASSWORD_FILE || null;
+const DB_USER_PASSWORD = DB_USER_PASSWORD_FILE ? fs.readFileSync(DB_USER_PASSWORD_FILE, 'utf8').trim() : "";
+
+const sequelize = new Sequelize(
+    DB_NAME,
+    DB_USER_NAME,
+    DB_USER_PASSWORD,
+    {
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: 'postgres',
+    }
+);
 
 module.exports = sequelize;
